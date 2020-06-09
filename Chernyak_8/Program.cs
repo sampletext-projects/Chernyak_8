70d8c25d22aee59ebb7eab504597ae91ac25a51c
sampletext32 <sampletext32@bk.ru>
2020-06-09 21:49:57 +0300

    Add project

---
+++ Chernyak_8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chernyak_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выполнила Черняк Оксана Александровна, АЭМ-111");
+            Console.WriteLine(Reverse(57));
+            Console.WriteLine(Reverse('a'));
+            Console.WriteLine(Reverse("abcd"));
+            Console.WriteLine(Reverse(173.12));
+            Console.WriteLine(Reverse(new object()));
+            Console.ReadKey();
+        }
+
+        private static string Reverse<T>(T obj)
+        {
+            if (obj == null) return "";
+            string str = obj.ToString();
+            string rev = "";
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                rev += str[i];
+            }
+
+            return rev;
+        }
+    }
+}
